634632b4aa882a62cbefbda8daa9f321233cff44
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-17 23:41:19 +0000

    Sort a doubly linked list with Insertion Sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -32,11 +32,12 @@ void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2)
  */
 void insertion_sort_list(listint_t **list)
 {    
-    listint_t *actual = *list, *key = NULL;    
+    listint_t *actual = NULL, *key = NULL;    
 
     if (*list == NULL)
         return;     
 
+    actual = *list;
     while (actual->next != NULL)
     {        
         key = actual->next;
