c16becd69767fd7311e990a949889c64848c7df4
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-18 01:02:24 +0000

    Sort a doubly linked list with Insertion Sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -34,7 +34,7 @@ void insertion_sort_list(listint_t **list)
 {    
     listint_t *actual = NULL, *key = NULL;    
 
-    if (!list || !(*list))
+    if (!list || !(*list) || !(*list)->next)
         return;     
 
     actual = *list;
