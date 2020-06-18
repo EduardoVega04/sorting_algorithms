1a6b11c7b3fdfa43bd9a63ddc7ed9c4cc3ab11ec
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-18 03:32:01 +0000

    Added quicksort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -34,7 +34,7 @@ void insertion_sort_list(listint_t **list)
 {    
     listint_t *actual = NULL, *key = NULL;    
 
-    if (!list || !(*list) || !(*list)->next)
+    if (!list || !(*list))
         return;     
 
     actual = *list;
